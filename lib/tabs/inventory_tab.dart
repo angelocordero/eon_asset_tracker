d40d71255f022ef014b0e10f44a26efa475dab14
angelo <johnangelocordero.dev@gmail.com>
2023-05-03 17:36:43 +0800

    try sticky table headers

--- lib/tabs/inventory_tab.dart
+++ lib/tabs/inventory_tab.dart
@@ -4,14 +4,32 @@ import 'package:eon_asset_tracker/core/utils.dart';
 import 'package:eon_asset_tracker/screens/add_item_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:table_sticky_headers/table_sticky_headers.dart';
+
+import '../models/item_model.dart';
 
 class InventoryTab extends ConsumerWidget {
   const InventoryTab({super.key});
 
-  static final ScrollController _scrollController = ScrollController();
+  //static final ScrollController _scrollController = ScrollController();
+
+  static final List<String> columns = [
+    'Asset ID',
+    'Item Model / Serial Number',
+    'Department',
+    'Person Accountable',
+    'Category',
+    'Price',
+    'Unit',
+    'Date Purchased',
+    'Date Received',
+    'Status',
+  ];
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    List<Item> rows = ref.watch(inventoryProvider);
+
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(
@@ -36,17 +54,47 @@ class InventoryTab extends ConsumerWidget {
           ),
           Flexible(
             flex: 3,
-            child: Scrollbar(
-              thumbVisibility: true,
-              child: SingleChildScrollView(
-                primary: true,
-                child: DataTable(
-                  columns: _columns(),
-                  rows: _rows(ref),
-                  showCheckboxColumn: false,
-                  showBottomBorder: true,
-                ),
-              ),
+            child: StickyHeadersTable(
+              showHorizontalScrollbar: true,
+              cellDimensions: const CellDimensions.variableColumnWidth(
+                  columnWidths: null,
+                  contentCellHeight: null,
+                  stickyLegendWidth: 200,
+                  stickyLegendHeight: 100),
+              columnsLength: columns.length,
+              rowsLength: rows.length,
+              columnsTitleBuilder: (i) => Text(columns[i]),
+              rowsTitleBuilder: (j) => Container(),
+              contentCellBuilder: (i, j) {
+                Item item = rows[j];
+
+                switch (i) {
+                  case 0:
+                    return Text(item.assetID);
+                  case 1:
+                    return Text(item.model);
+                  case 2:
+                    return Text(item.department);
+                  case 3:
+                    return Text(item.personAccountable.toString());
+                  case 4:
+                    return Text(item.category);
+                  case 5:
+                    return Text(item.price == null
+                        ? '0.00'
+                        : item.price!.toStringAsFixed(2));
+                  case 6:
+                    return Text(item.unit);
+                  case 7:
+                    return Text(dateToString(item.datePurchased!));
+                  case 8:
+                    return Text(dateToString(item.dateReceived!));
+                  case 9:
+                    return Text(item.status);
+                  default:
+                    return Container();
+                }
+              },
             ),
           ),
           const Divider(),
